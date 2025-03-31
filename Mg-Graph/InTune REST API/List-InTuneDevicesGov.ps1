3963fdbb35a2edabe98bd9e36c12351b14f5d572
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2025-03-31 18:32:33 -0400

    Update List-InTuneDevicesGov.ps1

--- Mg-Graph/InTune REST API/List-InTuneDevicesGov.ps1
+++ Mg-Graph/InTune REST API/List-InTuneDevicesGov.ps1
@@ -54,8 +54,8 @@ function List-InTuneDevices {
     # ------------------------------------------------------------------------
     # 4) Output the devices to CSV and JSON
     # ------------------------------------------------------------------------
-    $allDevices | Export-Csv -Path $CsvOutputPath -NoTypeInformation
-    $allDevices | ConvertTo-Json -Depth 25 | Out-File $JsonOutputPath -Encoding UTF8
+    $devices | Export-Csv -Path $CsvOutputPath -NoTypeInformation
+    $devices | ConvertTo-Json -Depth 25 | Out-File $JsonOutputPath -Encoding UTF8
 
     Write-Host "Devices exported to CSV:   $CsvOutputPath"
     Write-Host "Devices exported to JSON: $JsonOutputPath"
